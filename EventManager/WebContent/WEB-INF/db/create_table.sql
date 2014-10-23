f2cecb4649b2ecde6b338a09e41c25badd23ead1
Rémy Ferré <remy-ferre@laposte.net>
2014-10-23 17:59:31 +0200

    Modify database creation script

--- EventManager/WebContent/WEB-INF/db/create_table.sql
+++ EventManager/WebContent/WEB-INF/db/create_table.sql
@@ -1,16 +1,14 @@
-drop app.Subscriptions;
-drop app.Events;
-drop app.Users;
-
+drop table app.Subscriptions;
+drop table app.Events;
+drop table app.Users;
 
 create table app.Users (
 	email varchar(32) primary key,
 	password varchar(32) not null
-
 );
 
 create table app.Events  (
-	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+	id INTEGER primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
 	name varchar(32) not null,
 	address varchar(128) not null,
 	beginning_date timestamp not null,
@@ -20,9 +18,10 @@ create table app.Events  (
 );
 
 create table app.Subscriptions (
-	email varchar(32) not null primary key,
+	email varchar(32) not null,
 	first_name varchar(32) not null,
 	last_name varchar(32) not null,
 	company varchar(32) not null,
-	event integer references app.Events(uuid)
+	event integer references app.Events(id),
+	primary key (email, event)
 );
