d38002761541bd532a0bb4e20a96208c78892b15
Rémy Ferré <remy-ferre@laposte.net>
2014-10-26 11:25:49 +0100

    Verify the user is the real owner when publishing an event

--- EventManager/src/controllers/event/EventPublish.java
+++ EventManager/src/controllers/event/EventPublish.java
@@ -10,20 +10,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.demo.bean.jpa.EventsEntity;
+import org.demo.bean.jpa.UsersEntity;
 import org.demo.persistence.services.EventsPersistence;
 import org.demo.persistence.services.jpa.EventsPersistenceJPA;
+import org.demo.persistence.services.jpa.UsersPersistenceJPA;
 
 @WebServlet("/publish")
 public class EventPublish extends EventManagerServlet {
 
     public EventPublish() { super(); }
-
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userMail  = (String)request.getSession(false).getAttribute("token");
+		UsersEntity user = new UsersPersistenceJPA().load(userMail);
+    	
     	int eventId = Integer.parseInt(request.getParameter("eventId"));
     	EventsPersistence ep = new EventsPersistenceJPA();
     	EventsEntity event = ep.load(eventId);
-    	event.setPublished(1);
-    	ep.save(event);
-    	redirectTo("/event?id=" + eventId, request, response);
+    	
+    	if (user.getEmail().equals(event.getUsers().getEmail())) {
+    		event.setPublished(1);
+    		ep.save(event);
+    		redirectTo("/event?id=" + eventId, request, response);
+    	}
     }
 }
