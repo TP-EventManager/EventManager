50e78e189cc3c5e01765ede67640bc64a95a9049
Xartok <lagache.yoann@gmail.com>
2014-10-30 10:13:08 +0100

    Validate event id when showing the list of subscribers to an event

--- EventManager/src/controllers/subscription/SubscriptionList.java
+++ EventManager/src/controllers/subscription/SubscriptionList.java
@@ -18,8 +18,22 @@ import controllers.common.EventManagerServlet;
 public class SubscriptionList extends EventManagerServlet {
 	
     public SubscriptionList() { super(); }
+    
+    private int validateId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int eventId;
+		try { eventId = Integer.parseInt(request.getParameter("id")); }
+		catch (NumberFormatException e) {
+			renderView("event/EventNotFound.jsp", request, response);
+			return -1;
+		}
+		return eventId;
+    }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// validate id before using it
+		int eventIdInteger = validateId(request, response);
+		if (eventIdInteger == -1) return;
+		
 		String eventId = request.getParameter("id");
 		List<SubscriptionsEntity> subscriptions = new SubscriptionsListQuery().execute(eventId);
 		request.setAttribute("subscriptions", subscriptions);
